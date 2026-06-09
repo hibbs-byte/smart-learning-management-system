6fff710c512422ce7f020fb481b84b0570eefdb2
hibbs-byte <alihibban78@gmail.com>
2026-06-09 15:17:20 +0530

    Finalised Dashboard UI

---
+++ frontend/src/app/components/dashboard-stats/dashboard-stats.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-stats',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-stats.html',
+  styleUrl: './dashboard-stats.css',
+})
+export class DashboardStats {}
--- frontend/src/app/features/dashboard/dashboard/dashboard.ts
+++ frontend/src/app/features/dashboard/dashboard/dashboard.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 
+import { Sidebar } from '../../../components/sidebar/sidebar';
+import { Header } from '../../../components/header/header';
+import { DashboardStats } from '../../../components/dashboard-stats/dashboard-stats';
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
+  imports: [
+    Sidebar,
+    Header,
+    DashboardStats
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
